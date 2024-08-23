8456f9ecc6e9b51cff031a0b965e51ab2537ed5c
HugoNC <hugo.castro@comunix.net.br>
2024-08-23 14:57:45 -0300

    Create profile component
    Create input component using composition pattern

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,20 @@
+import { LogOut } from "lucide-react";
+
+export function Profile() {
+    return (
+        <div className="grid items-center gap-3 grid-cols-profile">
+            <img
+                src="https://github.com/HugoNCastro.png"
+                className="h-10 w-10 rounded-full"
+                alt=""
+            />
+            <div className="flex flex-col flex-1 truncate">
+                <span className="text-sm font-semibold text-zinc-700">Hugo Castro</span>
+                <span className="text-sm text-zinc-500 truncate">hnc@tech.com</span>
+            </div>
+            <button type="button" className="ml-auto p-2 hover:bg-zinc-50 rounded-md">
+                <LogOut className="w-5 h-5 text-zinc-500" />
+            </button>
+        </div>
+    )
+}

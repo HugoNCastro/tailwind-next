8274de0306d8db35ef654ad422c2b487f2c117a4
HugoNCastro <hugonaaman@gmail.com>
2024-08-22 22:30:39 -0300

    Create logo component
    Create NavItem component
    
    Create Widget component

--- src/components/Sidebar/Logo.tsx
+++ src/components/Sidebar/Logo.tsx
@@ -1,6 +1,6 @@
 export function Logo() {
   return (
-    <strong className="flex items-center gap-2 text-xl font-semibold text-zinc-900">
+    <strong className="flex items-center gap-2 text-xl font-semibold mx-1 text-zinc-900">
       <svg
         width={38}
         height={38}
